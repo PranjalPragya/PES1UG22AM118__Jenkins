557d2fb9cdecd82124f28cef256d2325f1857660
PranjalPragya <159558534+PranjalPragya@users.noreply.github.com>
2025-03-15 17:08:44 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Github push, this will run automatically  \n";
+  cout << "Github push, this will run automatically!!  \n";
   return 0;
 }
 //editting this file in the end to test the ngrok stuff
